898499f6bd18b13d8477dfac6aaa1eeb3dcee617
Maxime Faye <maxime.faye@gmail.com>
2013-04-04 08:15:40 +0200

    Implemented CRD on branch list

--- client/DeployHelper.js
+++ client/DeployHelper.js
@@ -4,9 +4,10 @@
  * Time: 19:30
  */
 var DeployHelper = {
-    sessionKey : 'currentDeploy',
+    currentDeployKey : 'currentDeploy',
+    branchListUpdateKey: 'branchListUpdate',
     getCurrentDeploy: function(){
-        var currentDeploy = Session.get(DeployHelper.sessionKey);
+        var currentDeploy = Session.get(DeployHelper.currentDeployKey);
         var out = null;
         if(currentDeploy && currentDeploy._id){
             out = currentDeploy;
@@ -15,6 +16,26 @@ var DeployHelper = {
         return out;
     },
     setCurrentDeploy : function(currentDeploy){
-        Session.set(DeployHelper.sessionKey, currentDeploy);
+        Session.set(DeployHelper.currentDeployKey, currentDeploy);
+    },
+
+    toggleBranchListUpdateState: function(){
+        var currentState = Session.get(this.branchListUpdateKey);
+        if(typeof currentState != 'undefined' && currentState != null){
+            currentState = !currentState;
+        }else{
+            currentState = true;
+        }
+        Session.set(this.branchListUpdateKey, currentState);
+    },
+
+    getBranchListUpdateState:function(){
+        var value = Session.get(this.branchListUpdateKey);
+        var out = null;
+        if(typeof value != 'undefined' && value != null){
+            out = value;
+        }
+
+        return out;
     }
 };
--- client/branches/branch_list.js
+++ client/branches/branch_list.js
@@ -5,6 +5,63 @@
  */
 Template.branchListDisplay.helpers({
     branchList : function(){
+        var out = null;
+        var currentDeploy = DeployHelper.getCurrentDeploy();
+        if(currentDeploy != null){
+            out = currentDeploy.branchList;
+        }
 
+        return out;
+    },
+    isUpdate: function(){
+        return DeployHelper.getBranchListUpdateState();
+    }
+});
+
+Template.branchListDisplay.events({
+    'click a#addBranch': function(event){
+        event.preventDefault();
+        var $selectProduct = $('#selectProduct');
+        var $branchName = $('#branchName');
+
+        var selectedProduct = $selectProduct.val();
+        var branchName = $branchName.attr('value');
+        var currentDeploy = DeployHelper.getCurrentDeploy();
+        if(selectedProduct != '' && branchName != '' && currentDeploy != null){
+            var branch = {_id: Meteor.uuid(),product: selectedProduct, branchName: branchName};
+            DeploymentList.update({_id: currentDeploy._id}, {$push: {branchList: branch}}, function(err){
+                if(err){
+                    throw err;
+                }
+                currentDeploy.branchList.push(branch);
+                DeployHelper.setCurrentDeploy(currentDeploy);
+                $selectProduct.val('');
+                $branchName.attr('value', '');
+            });
+        }
+    },
+    'click a#toggleUpdateMode': function(event){
+        event.preventDefault();
+        DeployHelper.toggleBranchListUpdateState();
+    },
+    'click a.removeBranchFromList': function(event){
+        event.preventDefault();
+        var currentDeploy = DeployHelper.getCurrentDeploy();
+        var self = this;
+        if(currentDeploy != null){
+            DeploymentList.find({_id: currentDeploy._id}).forEach(function(deploy){
+                var newBranchList = _.reject(deploy.branchList, function(element){
+                    return (element._id == self._id);
+                });
+                newBranchList = _.toArray(newBranchList);
+                DeploymentList.update({_id: currentDeploy._id}, {$set:{branchList: newBranchList}}, function(err){
+                    if(err){
+                        throw err;
+                    }
+                    currentDeploy.branchList = newBranchList;
+                    DeployHelper.setCurrentDeploy(currentDeploy);
+                });
+            });
+        }
     }
 });
